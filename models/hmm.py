16f6c65b78b2494f3f6b0ec1518decd0fc7ce884
gipbear <1099048889@qq.com>
2022-10-02 23:26:38 +0800

    学习 hmm 模型的建立过程

--- models/hmm.py
+++ models/hmm.py
@@ -3,9 +3,9 @@ import torch
 
 class HMM(object):
     def __init__(self, N, M):
-        """Args:
-            N: 状态数，这里对应存在的标注的种类
-            M: 观测数，这里对应有多少不同的字
+        """
+        :param int N: 状态数，这里对应存在的标注的种类
+        :param int M: 观测数，这里对应有多少不同的字
         """
         self.N = N
         self.M = M
@@ -18,14 +18,13 @@ class HMM(object):
         self.Pi = torch.zeros(N)
 
     def train(self, word_lists, tag_lists, word2id, tag2id):
-        """HMM的训练，即根据训练语料对模型参数进行估计,
-           因为我们有观测序列以及其对应的状态序列，所以我们
-           可以使用极大似然估计的方法来估计隐马尔可夫模型的参数
-        参数:
-            word_lists: 列表，其中每个元素由字组成的列表，如 ['担','任','科','员']
-            tag_lists: 列表，其中每个元素是由对应的标注组成的列表，如 ['O','O','B-TITLE', 'E-TITLE']
-            word2id: 将字映射为ID
-            tag2id: 字典，将标注映射为ID
+        """HMM 模型训练, 即根据训练语料对模型参数进行估计, 因为我们有观测序列以及其对应的状态序列, 
+            所以我们可以使用极大似然估计的方法来估计隐马尔可夫模型的参数。
+
+        :param list word_lists: 其中每个元素由字组成的列表, 如 ['担','任','科','员']
+        :param list tag_lists: 其中每个元素是由对应的标注组成的列表, 如 ['O','O','B-TITLE', 'E-TITLE']
+        :param dist word2id: 将字映射为ID
+        :param dict tag2id: 将标注映射为ID
         """
 
         assert len(tag_lists) == len(word_lists)
@@ -67,29 +66,32 @@ class HMM(object):
         return pred_tag_lists
 
     def decoding(self, word_list, word2id, tag2id):
-        """
-        使用维特比算法对给定观测序列求状态序列， 这里就是对字组成的序列,求其对应的标注。
-        维特比算法实际是用动态规划解隐马尔可夫模型预测问题，即用动态规划求概率最大路径（最优路径）
-        这时一条路径对应着一个状态序列
+        """使用Viterbi(维特比)算法对给定观测序列求状态序列, 这里就是对字组成的序列, 求其对应的标注。
+            维特比算法实际是用动态规划解隐马尔可夫模型预测问题,
+            即用动态规划求概率最大路径（最优路径）这时一条路径对应着一个状态序列
+
+        :param list word_list: 由字组成的列表, 如 ['担','任','科','员']
+        :param dist word2id: 将字映射为ID
+        :param dict tag2id: 将标注映射为ID
         """
         # 问题:整条链很长的情况下，十分多的小概率相乘，最后可能造成下溢
         # 解决办法：采用对数概率，这样源空间中的很小概率，就被映射到对数空间的大的负数
-        #  同时相乘操作也变成简单的相加操作
+        #          同时相乘操作也变成简单的相加操作
         A = torch.log(self.A)
         B = torch.log(self.B)
         Pi = torch.log(self.Pi)
 
-        # 初始化 维比特矩阵viterbi 它的维度为[状态数, 序列长度]
-        # 其中viterbi[i, j]表示标注序列的第j个标注为i的所有单个序列(i_1, i_2, ..i_j)出现的概率最大值
+        # 初始化维比特矩阵 viterbi 它的维度为[状态数, 序列长度]
+        # 其中 viterbi[i, j] 表示标注序列的第j个标注为i的所有单个序列(i_1, i_2, ..i_j)出现的概率最大值
         seq_len = len(word_list)
         viterbi = torch.zeros(self.N, seq_len)
-        # backpointer是跟viterbi一样大小的矩阵
-        # backpointer[i, j]存储的是 标注序列的第j个标注为i时，第j-1个标注的id
+        # backpointer 是跟 viterbi 一样大小的矩阵
+        # backpointer[i, j] 存储的是标注序列的第j个标注为i时，第j-1个标注的id
         # 等解码的时候，我们用backpointer进行回溯，以求出最优路径
         backpointer = torch.zeros(self.N, seq_len).long()
 
         # self.Pi[i] 表示第一个字的标记为i的概率
-        # Bt[word_id]表示字为word_id的时候，对应各个标记的概率
+        # Bt[word_id] 表示字为 word_id 的时候，对应各个标记的概率
         # self.A.t()[tag_id]表示各个状态转移到tag_id对应的概率
 
         # 所以第一步为
@@ -117,17 +119,12 @@ class HMM(object):
             else:
                 bt = Bt[wordid]  # 否则从观测概率矩阵中取bt
             for tag_id in range(len(tag2id)):
-                max_prob, max_id = torch.max(
-                    viterbi[:, step-1] + A[:, tag_id],
-                    dim=0
-                )
+                max_prob, max_id = torch.max(viterbi[:, step-1] + A[:, tag_id], dim=0)
                 viterbi[tag_id, step] = max_prob + bt[tag_id]
                 backpointer[tag_id, step] = max_id
 
         # 终止， t=seq_len 即 viterbi[:, seq_len]中的最大概率，就是最优路径的概率
-        best_path_prob, best_path_pointer = torch.max(
-            viterbi[:, seq_len-1], dim=0
-        )
+        best_path_prob, best_path_pointer = torch.max(viterbi[:, seq_len-1], dim=0)
 
         # 回溯，求最优路径
         best_path_pointer = best_path_pointer.item()
